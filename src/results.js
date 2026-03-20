7d4becae441a12fa93fa331873abb8cea75afaa4
Pezza242 <periyearwood@gmail.com>
2026-03-20 11:28:46 +0000

    Added results-box in results.js
    And decorated the box using CSS

--- src/results.js
+++ src/results.js
@@ -5,15 +5,18 @@ export default function Results(props) {
   if (props.data && props.data.meanings) {
     return (
       <div className="Results">
-        <h1 className="keyword">{props.data.word}</h1>
-        <h2>| {props.data.phonetic} |</h2>
-        {props.data.meanings.map(function (meanings, index) {
-          return (
-            <div key={index}>
-              <Meanings meaning={meanings} />
-            </div>
-          );
-        })}
+        <hr />
+        <section className="results-box">
+          <h1 className="keyword">{props.data.word}</h1>
+          <h2 className="phonetic">| {props.data.phonetic} |</h2>
+          {props.data.meanings.map(function (meanings, index) {
+            return (
+              <div key={index}>
+                <Meanings meaning={meanings} />
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   } else {
