500bd493a5da962b24857a49f3ae9f65324b3389
Pezza242 <periyearwood@gmail.com>
2026-03-20 12:39:44 +0000

    Added footer to app.js
    And updated a few bits

--- src/App.js
+++ src/App.js
@@ -7,6 +7,35 @@ function App() {
       <header className="App-header">
         <Dictionary />
       </header>
+      <footer>
+        Coded by{" "}
+        <a
+          className="footer-link"
+          href="https://github.com/Pezza242"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Peri Williams-Yearwood
+        </a>
+        ,{" "}
+        <a
+          className="footer-link"
+          href="https://github.com/Pezza242/my-dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on GitHub
+        </a>
+        , and{" "}
+        <a
+          className="footer-link"
+          href="https://pezza242-dictionary-project.netlify.app/?"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
--- src/meanings.js
+++ src/meanings.js
@@ -7,7 +7,9 @@ export default function Meanings(props) {
       <h3 className="part-of-speech">{props.meaning.partOfSpeech}</h3>
       <p className="definition">{props.meaning.definition}</p>
       {props.meaning.example ? (
-        <p className="example">"{props.meaning.example}"</p>
+        <p className="example">
+          <em>"{props.meaning.example}"</em>
+        </p>
       ) : null}
     </div>
   );
